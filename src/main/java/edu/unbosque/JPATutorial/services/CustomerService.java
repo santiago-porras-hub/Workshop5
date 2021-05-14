3d8485ee824e3ef186f11961545e7cbd5c25106d
santiago porras <sporrass@unbosque.edu.co>
2021-05-13 19:57:32 -0500

    Finishing Customer Service

--- src/main/java/edu/unbosque/JPATutorial/services/CustomerService.java
+++ src/main/java/edu/unbosque/JPATutorial/services/CustomerService.java
@@ -2,6 +2,7 @@ package edu.unbosque.JPATutorial.services;
 
 import edu.unbosque.JPATutorial.jpa.entities.Author;
 import edu.unbosque.JPATutorial.jpa.entities.Customer;
+import edu.unbosque.JPATutorial.jpa.repositories.AuthorRepositoryImpl;
 import edu.unbosque.JPATutorial.jpa.repositories.CustomerRepository;
 import edu.unbosque.JPATutorial.jpa.repositories.CustomerRepositoryImpl;
 import edu.unbosque.JPATutorial.servlets.pojos.AuthorPOJO;
@@ -19,23 +20,44 @@ public class CustomerService {
 
     CustomerRepository customerRepository;
 
-    public List<CustomerPOJO> listCustomers(){
+    //Gender ---> GENRE
+    public List<CustomerPOJO> listCustomers() {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tutorial");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        customerRepository = new CustomerRepositoryImpl(entityManager) {
+        customerRepository = new CustomerRepositoryImpl(entityManager);
             List<Customer> customers = customerRepository.findAll();
 
              entityManager.close();
         entityManagerFactory.close();
 
-            List<AuthorPOJO> authorsPOJO = new ArrayList<>();
-        for (
-            Customer customer : customer) {
-                authorsPOJO.add(new AuthorPOJO(
-                       customer.getEmail(),customer.getFirst_name()+" "+customer.getLast_name(),customer.getGenre(),customer.getAge()
+            List<CustomerPOJO> customerPOJO = new ArrayList<>();
+        for(
+            Customer customer :customers)
+
+            {
+                customerPOJO.add(new CustomerPOJO(
+                        customer.getEmail(), customer.getFirst_name() + " " + customer.getLast_name(),
+                        customer.getGenre(),
+                        customer.getAge()
                 ));
+            }
+        return customerPOJO;
 
         }
+
+        public Customer saveCustomer(String email, String first, String last, String genre, Integer age){
+
+            EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tutorial");
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+            customerRepository = new CustomerRepositoryImpl(entityManager);
+
+            Customer customer = new Customer(email,first,last,genre,age);
+            Customer persistedCustomer = customerRepository.save(customer).get();
+
+            entityManager.close();
+
+            return persistedCustomer;
+        }
     }
-}
