a2a23bea80cfe5524d4455dac4e2abf6e8dbd0e4
santiago porras <sporrass@unbosque.edu.co>
2021-05-18 23:11:39 -0500

    Update rent

--- src/main/java/edu/unbosque/JPATutorial/servlets/pojos/RentPOJO.java
+++ src/main/java/edu/unbosque/JPATutorial/servlets/pojos/RentPOJO.java
@@ -4,18 +4,13 @@ public class RentPOJO {
 
     private Integer rentId;
 
-    private String email;
+
 
     private String rentingDate;
 
 
     public  RentPOJO(){
 
-    }
-    public RentPOJO(Integer rentId, String email, String rentingDate) {
-        this.rentId = rentId;
-        this.email = email;
-        this.rentingDate = rentingDate;
     }
 
     public RentPOJO(Integer rentId, String rentingDate) {
@@ -29,14 +24,6 @@ public class RentPOJO {
 
     public void setRentId(Integer rentId) {
         this.rentId = rentId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getRentingDate() {
